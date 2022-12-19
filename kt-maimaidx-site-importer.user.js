040dd95a2984f6c40cc0300c201fc25e10cac0c3
j1nxie <rylieeeeexd@gmail.com>
2022-12-19 10:02:45 +0700

    wip: import recents

--- kt-maimaidx-site-importer.user.js
+++ kt-maimaidx-site-importer.user.js
@@ -69,41 +69,68 @@ function submitApiKey(event) {
 }
 
 function addNav() {
-	const topNode = document.querySelector(".comment_block.break.f_1.f_12")
+	const topNode = document.querySelectorAll(".comment_block.break.f_l.f_12")[0]
 	const hasApiKey = !!getApiKey()
-	let apiKeyText = `
-	<p style="color: #fff; text-align: center;">
-	  You don't have an API Key set up. Please \"Set up an API Key\" before proceeding.
-	</p>
-  `
+
+	const apiKeyParagraph = document.createElement("p")
+	let apiKeyText = "You don't have an API key set up. Please \"Set up an API Key\" before proceeding."
+
 	if (hasApiKey) {
 		apiKeyText = ""
 	}
-	let apiKeyLink = "<a id=\"setup-api-key-onclick\">Set up API Key (DO THIS FIRST)</a>"
-	if (hasApiKey) {
-		apiKeyLink = "<a id=\"setup-api-key-onclick\">Reconfigure API Key (if broken)</a>"
+
+	apiKeyParagraph.append(document.createTextNode(apiKeyText))
+	if (apiKeyText != "") {
+		apiKeyParagraph.append(document.createElement("br"))
 	}
-	const navHtml = `
-	<div style="background-color: #000">
-	  ${apiKeyText}
-	  <a href="/maimai-mobile/record">Jump to recent score import (preferred)</a><br />
-	  <a href="/maimai-mobile/record/musicGenre">Jump to PB import</a><br />
-	  ${apiKeyLink}
-	</div>
-  `
-	topNode.insertAdjacentHTML("afterend", navHtml)
+
+	let apiKeyLink = "Set up API Key (DO THIS FIRST)"
+
+	if (hasApiKey) {
+		apiKeyLink = "Reconfigure API Key (if broken)"
+	}
+
+	const apiKeySetup = document.createElement("a")
+	apiKeySetup.id = "setup-api-key-onclick"
+	apiKeySetup.append(document.createTextNode(apiKeyLink))
+
+	apiKeyParagraph.append(apiKeySetup)
+
+	const navHtml = document.createElement("div")
+	navHtml.append(apiKeyParagraph)
+
+	const navRecent = document.createElement("a")
+	navRecent.href = "/maimai-mobile/record"
+	const navRecentText = "Jump to recent score import (preferred)"
+	navRecent.append(navRecentText)
+	navRecent.append(document.createElement("br"))
+	navHtml.append(navRecent)
+
+	const navPb = document.createElement("a")
+	navPb.href = "/maimai-mobile/record/musicGenre"
+	const navPbText = "Jump to PB import"
+	navPb.append(navPbText)
+	navHtml.append(navPb)
+
+	topNode.append(navHtml)
 
 	document.querySelector("#setup-api-key-onclick").onclick = setupApiKey
 }
 
 function insertImportButton(message, onClick) {
-	const importButton = `
-	<a id="kt-import-button" style="box-shadow: 0 0 0 2px #FFF, 0 0 0 4px #9E9E9E; border-radius: 8px; background-color: #F31A7D; display: block; margin: 10px auto; padding: 5px; width: fit-content;">
-	  ${message}
-	</a>
-  `
-	const prevElem = document.querySelector(".playdata__tab") || document.querySelector(".common-nav")
-	prevElem.insertAdjacentHTML("afterend", importButton)
+	const importButton = document.createElement("a")
+	importButton.id = "kt-import-button"
+	importButton.style = "box-shadow: 0 0 0 2px #FFF, 0 0 0 4px #9E9E9E"
+	importButton.borderRadius = "8px"
+	importButton.backgroundColor = "#F31A7D"
+	importButton.display = "block"
+	importButton.margin = "10px auto"
+	importButton.padding = "5px"
+	importButton.width = "fit-content"
+	importButton.append(document.createTextNode(message))
+
+	const prevElem = document.querySelectorAll(".title")[0]
+	prevElem.insertAdjacentElement("afterend", importButton)
 
 	document.querySelector("#kt-import-button").onclick = onClick
 }
@@ -114,7 +141,7 @@ function updateStatus(message) {
 		statusElem = document.createElement("p")
 		statusElem.id = "kt-import-status"
 		statusElem.style = "text-align: center; background-color: #fff;"
-		const prevElem = document.querySelector(".playdata__tab") || document.querySelector(".common-nav")
+		const prevElem = document.querySelectorAll(".title")[0]
 		prevElem.insertAdjacentElement("afterend", statusElem)
 	}
 
@@ -232,13 +259,13 @@ function calculateLamp(totalLamp, score) {
 		"ap.png": "ALL PERFECT",
 		"applus.png": "ALL PERFECT+",
 	}
-	if totalLamp[1] === "fc_dummy" {
+	if (totalLamp[1] === "fc_dummy") {
 		let lamp = lampMap[totalLamp[0]]
 	} else {
 		let lamp = lampMap[totalLamp[1]]
 	}
 
-	if lamp === null {
+	if (lamp === null) {
 		lamp = "FAILED"
 	}
 
@@ -256,37 +283,37 @@ function extractDetailsInfo(liElem, expectedIcon) {
 }
 
 function executeRecentImport() {
-	const scoresElems = document.getElementsByClassName("p_10 t_l f_0 v_b");
+	const scoresElems = document.querySelectorAll(".p_10.t_l.f_0.v_b")
 	let scoresList = []
 
 	scoresElems.forEach(e => {
-		const title = e.querySelector(".basic_block.m_5.p_5.p_1_10.f_13.break").innerText
+		const title = e.querySelector(".basic_block.m_5.p_5.p_l_10.f_13.break").innerText
 
-		const style = e.querySelector(".playlog_music_kind_icon")[0].src
+		const style = e.querySelector(".playlog_music_kind_icon").src
 			.replace("https://maimaidx-eng.com/maimai-mobile/img/music_", "").replace(".png", "")
 		let difficultyName = e.querySelector(".playlog_top_container img")[0].src
 			.replace("https://maimaidx-eng.com/maimai-mobile/img/diff_", "").replace(".png", "")
-			.replace(difficultyName[0], difficultyName[0].toUpperCase())
+		difficultyName.replace(difficultyName[0], difficultyName[0].toUpperCase())
 
-		if style === "dx" {
+		if (style === "dx") {
 			difficultyName == "DX" + difficultyName
 		}
 
-		const scoreElem = e.querySelector(".playlog_achievement_txt.t_r")[0].innerHTML
+		const scoreElem = e.querySelector(".playlog_achievement_txt.t_r").innerHTML
 			.replace('<span class = "f_20"', '').replace("<span>", "")
 		const score = parseFloat(scoreElem.innerText.match(/[0-9]+.[0-9]+/)[0])
 
-		const clearStatus = e.querySelector(".basic_block.m_5.p_5.p_l.f_13.break")[0].src
+		const clearStatus = e.querySelector(".basic_block.m_5.p_5.p_l.f_13.break").src
 			.replace("https://maimaidx-eng.com/maimai-mobile/img/playlog/", "").replace(".png", "")
-		const lampStatus = e.querySelector(".playlog_result_innerblock.basic_block.p_5.f_13")[0].src
+		const lampStatus = e.querySelector(".playlog_result_innerblock.basic_block.p_5.f_13").src
 			.replace("https://maimaidx-eng.com/maimai-mobile/img/playlog/", "").replace(".png?ver=1.25", "")
 		const totalLamp = [clearStatus, lampStatus]
 		const lamp = calculateLamp(totalLamp, score)
 
-		const timestampElem = e.querySelector(".sub_title.t_c.f_r.f_11")[0].getElementByClassName("v_b")[1]
+		const timestampElem = e.querySelector(".sub_title.t_c.f_r.f_11").getElementByClassName("v_b")[1]
 		// Break out pieces, put into utc string with timezone info
 		const match = timestampElem.innerHTML.match("([0-9]{4})/([0-9]{1,2})/([0-9]{1,2}) ([0-9]{1,2}):([0-9]{2})")
-		let [_, year, month, day, hour, minute] = match
+		let [year, month, day, hour, minute] = match
 		if (month.length === 1) {
 			month = "0" + month
 		}
@@ -297,9 +324,8 @@ function executeRecentImport() {
 		if (hour.length === 1) {
 			hour = "0" + hour
 		}
-		const second = "00"
 		// Construct iso-8601 time
-		const isoTime = `${year}-${month}-${day}T${hour}:${minute}:${second}.000+09:00`
+		const isoTime = `${year}-${month}-${day}T${hour}:${minute}:00.000+09:00`
 		// Parse with Date, then get unix time
 		const date = new Date(isoTime)
 		const timeAchieved = date.valueOf()
@@ -322,7 +348,7 @@ function executeRecentImport() {
 			lamp,
 			matchType: "songTitle",
 			identifier: title,
-			difficulty,
+			difficulty: difficultyName,
 			timeAchieved,
 			judgements: {
 				pcrit,
@@ -421,19 +447,20 @@ function executeDanImport() {
 
 console.log("running")
 
-addNav()
-
 switch (location.pathname) {
-	case "/maimai-mobile/record/musicGenre":
+	case "/maimai-mobile/record/musicGenre/":
 		insertImportButton("IMPORT ALL PBs", warnPbImport)
 		break
 
-	case "/maimai-mobile/record":
+	case "/maimai-mobile/record/":
 		insertImportButton("IMPORT RECENT SCORES", executeRecentImport)
 		break
 
-	case "/maimai-mobile/home":
-	case "/maimai-mobile/playerData":
+	case "/maimai-mobile/home/":
+		addNav()
+		break
+
+	case "/maimai-mobile/playerData/":
 		insertImportButton("IMPORT STAGEUP", executeStageUpImport)
 		break
 }
